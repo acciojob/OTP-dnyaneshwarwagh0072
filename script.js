c6fe24f0163d3dbcfb3a402de05ada86a1d69070
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2025-02-07 15:10:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+    input.addEventListener("input", (e) => {
+        if (e.inputType === "insertText" && input.value) {
+            if (index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        }
+    });
+
+    input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && !input.value && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
+
